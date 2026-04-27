687d4cc97920111b2073ae5c55a18c996286d073
venilsonaquino <venilson2009@hotmail.com>
2026-04-27 14:52:59 -0300

    feat: impelementa repositorio para pedal, ponto e sessão

--- app/src/main/java/com/pedallog/app/domain/repository/PedalRepository.kt
+++ app/src/main/java/com/pedallog/app/domain/repository/PedalRepository.kt
@@ -5,14 +5,7 @@ import com.pedallog.app.domain.model.PedalSession
 import com.pedallog.app.domain.model.SessionId
 import kotlinx.coroutines.flow.Flow
 
-interface PedalRepository {
+interface PedalRepository : SessionRepository, PointRepository {
     suspend fun saveSessionWithPoints(session: PedalSession, points: List<PedalPoint>)
-    fun getAllSessions(): Flow<List<PedalSession>>
-    fun getPointsForSession(syncUuid: SessionId): Flow<List<PedalPoint>>
-    fun getPointsBySessionId(sessionId: Long): Flow<List<PedalPoint>>
-    suspend fun getSessionIdByUuid(syncUuid: SessionId): Long?
-    suspend fun sessionExists(syncUuid: SessionId): Boolean
-    suspend fun deleteSession(syncUuid: SessionId)
-
-    suspend fun deleteAllSessions()
 }
+
